91d4b24ab7142ebc70c0327b1734b5119d33dae4
CatPhat <kian@msoon.com>
2015-06-22 16:22:11 -0700

    update sql connection string

--- ItSynced.Web/DAL/EntityFramework/ItSyncedContext.cs
+++ ItSynced.Web/DAL/EntityFramework/ItSyncedContext.cs
@@ -13,7 +13,7 @@ namespace ItSynced.Web.DAL.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=GANESH;Database=ItSyncedDatabase;Trusted_Connection=True;MultipleActiveResultSets=true");
+                @"Server=GANESH\MDEVSQLSERVER;Database=ItSyncedDatabase;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
