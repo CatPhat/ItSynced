203a799688d1cd236c6683542aa19f6f191b6485
CatPhat <kian@msoon.com>
2015-06-22 17:29:19 -0700

    modify connection string

--- ItSynced.Web/DAL/EntityFramework/ItSyncedContext.cs
+++ ItSynced.Web/DAL/EntityFramework/ItSyncedContext.cs
@@ -13,7 +13,7 @@ namespace ItSynced.Web.DAL.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=GANESH;Database=ItSyncedDatabase;User Id=devuser;password=password123");
+                @"Server=GANESH\MDEVSQLSERVER;Database=ItSyncedDatabase;User Id=devuser;password=password123");
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
